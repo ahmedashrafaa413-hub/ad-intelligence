0955490e0a5df890943b7a3a78204db4f5a4b7f9
ahmedashrafaa413-hub <ahmed.ashraf.aa413@gmail.com>
2026-05-29 15:59:51 +0300

    Delete api/config.js

--- api/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    success: true,
-    service: "AdIntelligence Config",
-    meta_app_id_exists: !!process.env.META_APP_ID,
-    supabase_url_exists: !!process.env.SUPABASE_URL
-  });
-};
