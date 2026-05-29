0f398ecc021274e51626a5b5cfbf172d75dac5a6
ahmedashrafaa413-hub <ahmed.ashraf.aa413@gmail.com>
2026-05-29 17:49:39 +0300

    Delete api/config.js

--- api/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    success: true,
-    meta_app_id: process.env.META_APP_ID || null,
-    supabase_connected: !!process.env.SUPABASE_URL
-  });
-};
