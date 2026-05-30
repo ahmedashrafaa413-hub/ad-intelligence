7aa6363f97e26701ea3d705ec3daf1fcf8490ca9
ahmedashrafaa413-hub <ahmed.ashraf.aa413@gmail.com>
2026-05-30 15:21:04 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
-import SetupPage from "../setup/SetupPage";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return <SetupPage />;
+  redirect("/dashboard");
 }
