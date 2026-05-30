b68ecaf25163a60ab642e20de5fcbdb0f8786b91
ahmedashrafaa413-hub <ahmed.ashraf.aa413@gmail.com>
2026-05-30 14:16:33 +0300

    Update page.jsx

--- app/connections/page.jsx
+++ app/connections/page.jsx
@@ -1,8 +1,5 @@
+import SetupPage from "../../../setup/SetupPage";
+
 export default function ConnectionsPage() {
-  return (
-    <div className="page-shell">
-      <h1>Connections Center</h1>
-      <p>Manage all connected advertising platforms.</p>
-    </div>
-  );
+  return <SetupPage />;
 }
